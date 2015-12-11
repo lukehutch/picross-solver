9719f1ea890215210c076d964c499ef319910106
Luke Hutchison <luke.hutch@gmail.com>
2015-12-11 03:42:04 -0800

    Remove webpbn code

--- src/picross/PicrossSolver.java
+++ src/picross/PicrossSolver.java
@@ -1,14 +1,7 @@
 package picross;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -38,58 +31,6 @@ public class PicrossSolver {
                 throw new RuntimeException("Wrong dimensions");
             }
         }
-
-        // Solve a problem on http://webpbn.com/
-        public Problem(int webpbnProbNum) {
-            try {
-                Pattern countMatcher = Pattern.compile("<count>([0-9]+)</count>");
-                StringBuffer result = new StringBuffer();
-                URL url = new URL("http://webpbn.com/XMLpuz.cgi?id=" + webpbnProbNum);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                ArrayList<String> rowRuns = new ArrayList<>(), colRuns = new ArrayList<>();
-                boolean colLines = false;
-                try (BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                    for (String line; (line = rd.readLine()) != null;) {
-                        if (line.equals("<clues type=\"columns\">")) {
-                            colLines = true;
-                        } else if (line.equalsIgnoreCase("<clues type=\"rows\">")) {
-                            colLines = false;
-                        } else if (line.startsWith("<line>")) {
-                            StringBuilder run = new StringBuilder();
-                            Matcher matcher = countMatcher.matcher(line);
-                            while (matcher.find()) {
-                                int num = Integer.parseInt(matcher.group(1));
-                                if (num <= 0) {
-                                    throw new RuntimeException("Run length is zero or negative");
-                                }
-                                // TODO: Should probably encode these using ints, rather than chars
-                                if (num < 10) {
-                                    run.append("" + num);
-                                } else {
-                                    char chr = (char) ('A' + num - 10);
-                                    // if (chr > 126) {
-                                    //     throw new RuntimeException("Run length too big");
-                                    // }
-                                    run.append(chr);
-                                }
-                            }
-                        }
-                        result.append(line);
-                    }
-                }
-                // TODO
-                this.numRows = this.numCols = 0;
-                this.rowRuns = this.colRuns = null;
-                this.initialConstraints = null;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    static {
-        new Problem(12548);
     }
 
     Problem problem;
